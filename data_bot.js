86798da9e40ac4393441a7ce685d1a6224ec7a6f
Sam Liang <sam@Sams-MacBook-Pro.local>
2021-10-24 00:01:21 +0800

    added data bot to track monthly spending and target

---
+++ data_bot.js
@@ -0,0 +1,89 @@
+require('dotenv').config()
+const fs = require('fs');
+const { CanvasRenderService } = require('chartjs-node-canvas');
+
+const Telegraf = require('telegraf').Telegraf;
+const data_bot = new Telegraf(process.env.TELEGRAM_DATA_BOT_TOKEN);
+const DBSOURCE = process.env.DBSOURCE
+const sqlite3 = require('sqlite3').verbose();
+const {
+    read_month_expenses_sql,
+    group_by,
+    get_sum,
+    insert_data_to_db,
+    insert_target_sql,
+    read_db_data,
+    get_month_target_sql,
+    get_current_month
+} = require('./utils.js')
+
+const months = {
+    'jan' : '01',
+    'feb' : '02',
+    'mar' : '03',
+    'apr' : '04',
+    'may' : '05',
+    'jun' : '06',
+    'jul' : '07',
+    'aug' : '08',
+    'sep' : '09',
+    'oct' : '10',
+    'nov' : '11',
+    'dec' : '12'
+}
+
+let db = new sqlite3.Database(DBSOURCE, sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message)
+        return
+    }
+    console.log('Connected to SQlite database.');
+});
+
+data_bot.hears(/^target\W+(\d+)$/, async ctx => {
+    const set_target_sql = insert_target_sql()
+    const target = ctx.match[1].trim()
+    const current_month = get_current_month()
+    const data = []
+    data_bot.telegram.sendMessage(ctx.chat.id, 'bribing locksmith')
+    await insert_data_to_db(db, set_target_sql, [current_month, target])
+    data_bot.telegram.sendMessage(ctx.chat.id, '*hiding the gold inside the cloak*')
+})
+
+data_bot.hears(/^jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec$/, async ctx => {
+    const month = months[ctx.match[0].trim()] + "/" + new Date().getFullYear().toString()
+    const get_by_month_command = read_month_expenses_sql()
+    const expense_details_by_month = await get_expense_by_month(get_by_month_command, month, ctx)
+    const amount_sum_by_month = get_sum(expense_details_by_month)
+    const category_details = group_by(expense_details_by_month, 'Category')
+    var category_details_copy = JSON.parse(JSON.stringify(category_details));
+    for (const [category, items] of Object.entries(category_details_copy)) {
+        var sum_total_int = get_sum(items)
+        category_details_copy[category] = sum_total_int
+    }
+    const month_target_sql = get_month_target_sql()
+    const current_month = get_current_month()
+    const current_month_target = await get_current_month_target(month_target_sql, current_month, ctx)
+    data_bot.telegram.sendMessage(ctx.chat.id, `You have spent ${amount_sum_by_month}/${current_month_target} in ${month}`)
+    data_bot.telegram.sendMessage(ctx.chat.id,
+        `<b>Auto</b>: $${category_details_copy['Auto']}, ${category_details['Auto'].length}, ${(100*(category_details_copy['Auto']/amount_sum_by_month)).toFixed(2)}%\n<b>Food</b>: $${category_details_copy['Food']}, ${category_details['Food'].length}, ${(100*(category_details_copy['Food']/amount_sum_by_month)).toFixed(2)}%\n<b>交通</b>: $${category_details_copy['交通']}, ${category_details['交通'].length}, ${(100*(category_details_copy['交通']/amount_sum_by_month)).toFixed(2)}%\n<b>日常用品</b>: $${category_details_copy['日常用品']}, ${category_details['日常用品'].length}, ${(100*(category_details_copy['日常用品']/amount_sum_by_month)).toFixed(2)}%\n<b>餐厅</b>: $${category_details_copy['餐厅']}, ${category_details['餐厅'].length}, ${(100*(category_details_copy['餐厅']/amount_sum_by_month)).toFixed(2)}%`, {parse_mode: 'HTML'}
+    )
+})
+
+async function get_expense_by_month(get_by_month_command, month, ctx) {
+    data_bot.telegram.sendMessage(ctx.chat.id, 'bribing locksmith')
+    const expenses = await read_db_data(db, get_by_month_command, month)
+    data_bot.telegram.sendMessage(ctx.chat.id, '*hiding the gold inside the cloak*')
+    return expenses
+}
+
+async function get_current_month_target(month_target_sql, current_month, ctx) {
+    var target = await read_db_data(db, month_target_sql, current_month)
+    target = target[0]['Target']
+    return target
+}
+
+data_bot.launch();
+
+process.once('SIGINT', () => data_bot.stop('SIGINT'))
+process.once('SIGTERM', () => data_bot.stop('SIGTERM'))
---
+++ utils.js
@@ -0,0 +1,104 @@
+function read_month_expenses_sql() {
+    return "select * from Expenses where strftime('%m/%Y', strftime('%Y-%m-%d',datetime(substr(Date, 7, 4) || '-' || substr(Date, 4, 2) || '-' || substr(Date, 1, 2)))) is ?"
+}
+
+function insert_expense_sql() {
+    return "INSERT INTO Expenses (Amount, Description, Category, Date) VALUES(?,?,?,?)"
+}
+
+function insert_target_sql() {
+    return "INSERT INTO Targets (Date, Target) VALUES(?, ?);"
+}
+
+function get_month_target_sql() {
+    return "select * from Targets where Date like ?;"
+}
+
+async function insert_data_to_db(db, sql_command, params) {
+    await db.run(sql_command, params, (err, rows) => {
+        if (err) {
+            console.log(err.message)
+            return;
+        }
+    })
+}
+
+function read_db_data(db, sql_command, params) {
+    data = []
+    return new Promise(resolve => {
+        db.all(sql_command, params, function(err, rows) {
+            if (err) {
+                console.log(err.message)
+                return;
+            }
+            rows.forEach((row)=>{
+                data.push(row);
+            });
+
+            resolve(data);
+        });
+    })
+}
+
+var group_by = function(xs, key) {
+    return xs.reduce(function(rv, x) {
+        (rv[x[key]] = rv[x[key]] || []).push(x);
+        return rv;
+    }, {});
+};
+
+var get_sum = function(list) {
+    return list.reduce((partial_sum, current) => partial_sum + current['Amount'], 0).toFixed(2)
+}
+
+function get_current_date() {
+    var today = new Date()
+    today = today.toLocaleDateString('en-SG')
+    return today
+}
+
+function get_yesterday() {
+    var today = new Date()
+    var yesterday = new Date(today)
+    yesterday.setDate(yesterday.getDate() - 1)
+    yesterday = yesterday.toLocaleDateString('en-SG')
+    return yesterday
+}
+
+function get_tomorrow() {
+    var today = new Date()
+    var tomorrow = new Date(today)
+    tomorrow.setDate(tomorrow.getDate() + 1)
+    tomorrow = tomorrow.toLocaleDateString('en-GB')
+    return tomorrow
+}
+
+function get_current_month() {
+    var today = new Date()
+    today = today.toLocaleDateString('en-SG')
+    today = today.substr(3)
+    return today
+}
+
+function get_current_week() {
+
+}
+
+function get_last_7_days() {
+
+}
+
+module.exports = {
+    get_tomorrow,
+    get_yesterday,
+    get_current_date,
+    get_current_month,
+    read_month_expenses_sql,
+    group_by,
+    get_sum,
+    insert_expense_sql,
+    insert_data_to_db,
+    insert_target_sql,
+    read_db_data,
+    get_month_target_sql
+};
